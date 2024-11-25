9e29c02b90c5741a28843cf1ea6b689b40623051
Lena Cortes <lena.cortes@usal.es>
2024-11-25 16:36:20 +0100

    first day completed

--- src/question.js
+++ src/question.js
@@ -1,7 +1,19 @@
 class Question {
     // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+    constructor (text, choices, answer, difficulty) {
+        this.text = text; //string
+        this.choices = choices; // arr of string [1,2 ,3, 4,5]
+        this.answer = answer; //srting
+        this.difficulty = difficulty; // number 1 -3 (easienst to hardest)
+    }
 
-    // 2. shuffleChoices()
+    shuffleChoices() {
+
+        for (let i = this.choices.length - 1; i > 0; i--) { 
+            const j = Math.floor(Math.random() * this.choices.length); 
+            [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]]; 
+          } 
+        return this.choices; 
+        
+    }
 }
