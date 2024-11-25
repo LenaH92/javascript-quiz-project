394721477d6705551b98cf24d619bad114fa24ee
Lena Cortes <lena.cortes@usal.es>
2024-11-25 17:13:03 +0100

    now for real day 1 done

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,48 @@
 class Quiz {
     // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
 
-    // 2. getQuestion()
+    constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions; //array
+        this.timeLimit = timeLimit; //number
+        this.timeRemaining = timeRemaining; // number
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
+
+    getQuestion() {
+        
+        return this.questions[this.currentQuestionIndex]; 
+  
+    }
     
-    // 3. moveToNextQuestion()
+    moveToNextQuestion() {
+        this.currentQuestionIndex += 1;
+    }
 
-    // 4. shuffleQuestions()
+    shuffleQuestions() {
+        for (let i = this.questions.length - 1; i > 0; i--) { 
+            const j = Math.floor(Math.random() * this.questions.length); 
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]]; 
+        }
+    }
 
-    // 5. checkAnswer(answer)
+    checkAnswer(answer) {
+        this.correctAnswers += 1;
+    }
 
-    // 6. hasEnded()
+    hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length){
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
+
+/* 
+const test [
+    "what day is it today",
+    "other question",
+    "whatever else",
+    ...
+] */
